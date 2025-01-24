198b56ab4ec859e9ed6c48dfe6be37f3995fa7ed
Omer Dotchamou <omer.dotchamou@scalens.com>
2025-01-24 10:01:40 +0100

    Updated toast message style

--- src/components/ContactFormModal.jsx
+++ src/components/ContactFormModal.jsx
@@ -21,7 +21,14 @@ function ContactFormModal({ isOpen, setIsOpen }) {
     e.preventDefault();
     console.log("Données soumises :", formData);
 
-    toast.success("Message envoyé avec succès !", { position: "top-right" });
+    toast.success("Message envoyé avec succès !", {
+      position: "top-right",
+      style: {
+        width: "90%",
+        maxWidth: "400px",
+        margin: "5px",
+      },
+    });
 
     setFormData({ email: "", subject: "", message: "" });
     setIsOpen(false);
@@ -71,7 +78,7 @@ function ContactFormModal({ isOpen, setIsOpen }) {
                   id="email"
                   value={formData.email}
                   onChange={handleChange}
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary focus:outline-primary block w-full p-2.5"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary focus:border-primary focus:outline-primary block w-full p-2.5"
                   placeholder="nom@exemple.com"
                   required
                 />
@@ -88,7 +95,7 @@ function ContactFormModal({ isOpen, setIsOpen }) {
                   id="subject"
                   value={formData.subject}
                   onChange={handleChange}
-                  className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary focus:border-primary focus:outline-primary"
+                  className="block p-3 w-full text-base text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary focus:border-primary focus:outline-primary"
                   placeholder="Expliquez brièvement votre sujet"
                   required
                 />
@@ -105,7 +112,7 @@ function ContactFormModal({ isOpen, setIsOpen }) {
                   rows="6"
                   value={formData.message}
                   onChange={handleChange}
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary focus:border-primary focus:outline-primary"
+                  className="block p-2.5 w-full text-base text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary focus:border-primary focus:outline-primary"
                   placeholder="Décrivez votre besoin ou votre message..."
                   required
                 ></textarea>
---
+++ src/constants/images.js
@@ -0,0 +1,29 @@
+import customer1 from "../assets/customer-1.png";
+import customer2 from "../assets/customer-2.webp";
+import development from "../assets/development.jpg";
+import ecommerce1 from "../assets/ecommerce-1.webp";
+import ecommerce2 from "../assets/ecommerce-2.webp";
+import ecommerce3 from "../assets/ecommerce-3.webp";
+import network from "../assets/network.jpg";
+import iot from "../assets/iot.jpg";
+import kiosk from "../assets/kiosk.webp";
+import saas from "../assets/sass-invoice.webp";
+import office from "../assets/office.png";
+import invoice from "../assets/sass-invoice.webp";
+import smoothbill from "../assets/smoothbill.png";
+
+export const {
+  customer1,
+  customer2,
+  development,
+  ecommerce1,
+  ecommerce2,
+  ecommerce3,
+  network,
+  iot,
+  kiosk,
+  saas,
+  office,
+  invoice,
+  smoothbill,
+} = images;
